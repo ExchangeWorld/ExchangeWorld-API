cc972d74d167804bff2f61f4f96b7e08c866eb4c
NoelBao <noeleon930@gmail.com>
2015-09-13 14:20:59 +0800

    refactor user

--- routers/user.js
+++ routers/user.js
@@ -31,20 +31,14 @@ router.get('/', function(req, res, next) {
 	}
 
 	// Emit a find operation with orm in table `users`
-	users
-		.sync({
-			force: false
-		})
-		.then(function() {
-			return users.findAll({
-				where: {
-					$or: [{
-						uid: _uid
-					}, {
-						fb_id: _fb_id
-					}]
-				}
-			});
+	users.findAll({
+			where: {
+				$or: [{
+					uid: _uid
+				}, {
+					fb_id: _fb_id
+				}]
+			}
 		})
 		.then(function(result) {
 			res.json(result);
@@ -68,21 +62,20 @@ router.post('/register', function(req, res, next) {
 	var _photo_path = req.body.photo_path;
 
 	// Create instance
-	users
-		.sync({force: false})
-		.then(function() {
-			return users.create({
-				fb_id      : _fb_id,
-				name       : _name,
-				email      : _email,
-				photo_path : _photo_path
-			});
+	users.create({
+			fb_id: _fb_id,
+			name: _name,
+			email: _email,
+			photo_path: _photo_path
+
 		})
 		.then(function(result) {
 			res.json(result);
 		})
 		.catch(function(err) {
-			res.send({error: err});
+			res.send({
+				error: err
+			});
 			//res.json({});
 		});
 });
