e204fbb19b7eaa82d59da9c5e7a6be835dce9e90
NoelBao <noeleon930@gmail.com>
2015-11-01 07:20:12 +0800

    refactor goods

--- routers/goods.js
+++ routers/goods.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 // Including tables
-var goods    = require('../ORM/Goods');
-var users    = require('../ORM/Users');
+var goods = require('../ORM/Goods');
+var users = require('../ORM/Users');
 var comments = require('../ORM/Comments');
 
 // Get a good by given goods_gid
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
 
 	// Available query params:
 	//
@@ -17,28 +17,29 @@ router.get('/', function(req, res, next) {
 	// Get property:value in ?x=y&z=w....
 	var _gid = parseInt(req.query.gid, 10);
 
-	// Set association between tables (users, goods) and (goods, comments)
-	users.hasMany(goods, {foreignKey: 'owner_uid'});
-	goods.belongsTo(users, {foreignKey: 'owner_uid'});
-	goods.hasMany(comments, {foreignKey: 'goods_gid'});
-	comments.belongsTo(goods, {foreignKey: 'goods_gid'});
-
 	// Emit a find operation with orm model in table `goods`
-	goods.findAll({
+	goods
+		.findAll({
 			where: {
 				gid: _gid
 			},
+			// include: [{
+			// 	model: users,
+			// 	as: 'owner',
+			// 	required: true
+			// }, {
+			// 	model: comments,
+			// 	as: 'comments',
+			// 	required: true
+			// }]
 			include: [{
-				model: users,
-				required: true
-			}, {
-				model: comments
+				all: true
 			}]
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
+		.catch(err => {
 			res.send({
 				error: err
 			});
@@ -46,7 +47,7 @@ router.get('/', function(req, res, next) {
 });
 
 // Get goods by given owner_uid
-router.get('/of', function(req, res, next) {
+router.get('/of', (req, res) => {
 
 	// Available query params:
 	//
@@ -57,15 +58,24 @@ router.get('/of', function(req, res, next) {
 	var _owner_uid = parseInt(req.query.owner_uid, 10);
 
 	// Emit a find operation with orm model in table `goods`
-	goods.findAll({
+	goods
+		.findAll({
 			where: {
 				owner_uid: _owner_uid
-			}
+			},
+			// include: [{
+			// 	model: comments,
+			// 	as: 'comments',
+			// 	required: true
+			// }]
+			include: [{
+				all: true
+			}]
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
+		.catch(err => {
 			res.send({
 				error: err
 			});
@@ -73,7 +83,7 @@ router.get('/of', function(req, res, next) {
 });
 
 // Post a good
-router.post('/post', function(req, res, next) {
+router.post('/post', (req, res) => {
 
 	// Necessary POST body params:
 	//
@@ -87,16 +97,17 @@ router.post('/post', function(req, res, next) {
 	//
 
 	// Get property:value in POST body
-	var _name        = req.body.name;
-	var _category    = req.body.category;
+	var _name = req.body.name;
+	var _category = req.body.category;
 	var _description = req.body.description || '';
-	var _photo_path  = req.body.photo_path || '';
-	var _position_x  = parseFloat(req.body.position_x);
-	var _position_y  = parseFloat(req.body.position_y);
-	var _owner_uid   = parseInt(req.body.owner_uid, 10);
+	var _photo_path = req.body.photo_path || '';
+	var _position_x = parseFloat(req.body.position_x);
+	var _position_y = parseFloat(req.body.position_y);
+	var _owner_uid = parseInt(req.body.owner_uid, 10);
 
 	// Create instance
-	goods.create({
+	goods
+		.create({
 			name: _name,
 			category: _category,
 			description: _description,
@@ -105,10 +116,10 @@ router.post('/post', function(req, res, next) {
 			position_y: _position_y,
 			owner_uid: _owner_uid
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
+		.catch(err => {
 			res.send({
 				error: err
 			});
@@ -116,7 +127,7 @@ router.post('/post', function(req, res, next) {
 });
 
 // Edit a good
-router.put('/edit', function(req, res, next) {
+router.put('/edit', (req, res) => {
 
 	// Necessary PUT body params:
 	//
@@ -130,22 +141,23 @@ router.put('/edit', function(req, res, next) {
 	//
 
 	// Get property:value in PUT body
-	var _gid         = parseInt(req.body.gid, 10);
-	var _name        = req.body.name;
-	var _category    = req.body.category;
+	var _gid = parseInt(req.body.gid, 10);
+	var _name = req.body.name;
+	var _category = req.body.category;
 	var _description = req.body.description || '';
-	var _photo_path  = req.body.photo_path || '';
-	var _position_x  = parseFloat(req.body.position_x);
-	var _position_y  = parseFloat(req.body.position_y);
+	var _photo_path = req.body.photo_path || '';
+	var _position_x = parseFloat(req.body.position_x);
+	var _position_y = parseFloat(req.body.position_y);
 
 
 	// Find the good which got right gid and update values
-	goods.findOne({
+	goods
+		.findOne({
 			where: {
 				gid: _gid
 			}
 		})
-		.then(function(result) {
+		.then(result => {
 			if (result == null) {
 				return {};
 			} else {
@@ -155,14 +167,14 @@ router.put('/edit', function(req, res, next) {
 				result.photo_path = _photo_path;
 				result.position_x = _position_x;
 				result.position_y = _position_y;
-				result.save().then(function() {});
+				result.save().then(() => {});
 				return result;
 			}
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
+		.catch(err => {
 			res.send({
 				error: err
 			});
@@ -170,7 +182,7 @@ router.put('/edit', function(req, res, next) {
 });
 
 // Rate a good
-router.put('/rate', function(req, res, next) {
+router.put('/rate', (req, res) => {
 
 	// Necessary PUT body params:
 	//
@@ -178,29 +190,30 @@ router.put('/rate', function(req, res, next) {
 	// rate
 	//
 
-	var _gid  = parseInt(req.body.gid, 10);
+	var _gid = parseInt(req.body.gid, 10);
 	var _rate = parseFloat(req.body.rate);
 
 	if (_rate != _rate) {
-		res.json({
+		res.send({
 			error: 'rate is NaN'
 		});
 	}
 
-	goods.findOne({
+	goods
+		.findOne({
 			where: {
 				gid: _gid
 			}
 		})
-		.then(function(_goods) {
+		.then(_goods => {
 			_goods.rate = _rate;
-			_goods.save().then(function() {});
+			_goods.save().then(() => {});
 			return _goods;
 		})
-		.then(function(_goods) {
+		.then(_goods => {
 			res.json(_goods);
 		})
-		.catch(function(err) {
+		.catch(err => {
 			res.json({
 				error: err
 			});
@@ -208,7 +221,7 @@ router.put('/rate', function(req, res, next) {
 })
 
 // Delete a good (but not really delete it)
-router.delete('/delete', function(req, res, next) {
+router.delete('/delete', (req, res) => {
 
 	// Necessary query params:
 	//
@@ -218,22 +231,28 @@ router.delete('/delete', function(req, res, next) {
 	// Get property:value in DELETE query
 	var _gid = parseInt(req.query.gid, 10);
 
-	goods.findOne({
+	goods
+		.findOne({
 			where: {
 				gid: _gid
 			}
 		})
-		.then(function(result) {
+		.then(result => {
 			if (result == null) {
 				return {};
 			} else {
 				result.deleted = 1;
-				result.save().then(function() {});
+				result.save().then(() => {});
 				return result;
 			}
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
+		})
+		.catch(err => {
+			res.json({
+				error: err
+			});
 		});
 });
 
