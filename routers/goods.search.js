f05eb871adb4776ac1c04e08b21684cd3fbe852d
NoelBao <noeleon930@gmail.com>
2015-11-01 07:27:23 +0800

    refactor goods.search.js

--- routers/goods.search.js
+++ routers/goods.search.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 // Including tables
-var goods   = require('../ORM/Goods');
-var users   = require('../ORM/Users');
+var goods = require('../ORM/Goods');
+var users = require('../ORM/Users');
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
 
 	// Available query params:
 	//
@@ -20,62 +20,68 @@ router.get('/', function(req, res, next) {
 	// to
 	//
 
-	var name       = req.query.name || '';
-	var wishlist   = req.query.wishlist || '';
-	var category   = req.query.category || '';
+	var name = req.query.name || '';
+	var wishlist = req.query.wishlist || '';
+	var category = req.query.category || '';
 	var position_x = parseFloat(req.query.position_x) || -1.0;
 	var position_y = parseFloat(req.query.position_y) || -1.0;
-	var bound      = req.query.bound || '-90,0,90,180';
-	var from       = parseInt(req.query.from, 10) || -1;
-	var to         = parseInt(req.query.to, 10) || -1;
+	var bound = req.query.bound || '-90,0,90,180';
+	var from = parseInt(req.query.from, 10) || -1;
+	var to = parseInt(req.query.to, 10) || -1;
 
-	/**
+	/*
 	 * [ lat_lo, lng_lo, lat_hi, lng_hi ] for this bounds,
 	 *   where "lo" corresponds to the southwest corner of the bounding box,
 	 *   while "hi" corresponds to the northeast corner of that box.
 	 */
+
 	var boundArray = bound.split(',').map(parseFloat);
 
-	// Set association between tables (users, goods)
-	users.hasMany(goods, {foreignKey:'owner_uid'});
-	goods.belongsTo(users, {foreignKey: 'owner_uid'});
-
 	// Emit a find operation with orm model in table `goods`
-	goods.findAll({
-		where: {
-			$and: [{
-				name: (name === '' ? {
-					$ilike: '%'
-				} : {
-					$ilike: '%' + name + '%'
-				}),
-				category: (category === '' ? {
-					$ilike: '%'
-				} : {
-					$ilike: '%' + category + '%'
-				})
-			}],
-			position_x: {
-				gt: boundArray[1],
-				lt: boundArray[3]
+	goods
+		.findAll({
+			where: {
+				$and: [{
+					name: (name === '' ? {
+						$ilike: '%'
+					} : {
+						$ilike: '%' + name + '%'
+					}),
+					category: (category === '' ? {
+						$ilike: '%'
+					} : {
+						$ilike: '%' + category + '%'
+					})
+				}],
+				position_x: {
+					gt: boundArray[1],
+					lt: boundArray[3]
+				},
+				position_y: {
+					gt: boundArray[0],
+					lt: boundArray[2]
+				},
+				exchanged: {
+					$in: [0, 2]
+				},
+				deleted: 0
 			},
-			position_y: {
-				gt: boundArray[0],
-				lt: boundArray[2]
-			},
-			status: {
-				$in: [0, 2]
-			},
-			deleted: 0
-		},
-		include: [{
-			model: users,
-			required: true
-		}]
-	})
-	.then(function(result) {
-		res.json(result);
-	});
+			// include: [{
+			// 	model: users,
+			// 	required: true
+			// }]
+			include: [{
+				all: true
+			}]
+		})
+		.then(result => {
+			res.json(result);
+		})
+		.catch(err => {
+			res.send({
+				error: err
+			});
+		});
 });
 
 module.exports = router;
