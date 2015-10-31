7de533f91e590cfcdf9db5d17c455b5df0792acd
NoelBao <noeleon930@gmail.com>
2015-11-01 06:01:12 +0800

    refactor comment

--- routers/comment.js
+++ routers/comment.js
@@ -1,157 +1,163 @@
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 // Including tables
 var comments = require('../ORM/Comments');
-var users    = require('../ORM/Users');
+var users = require('../ORM/Users');
 
 // Get comments of a goods
-router.get('/of/goods', function(req, res, next) {
+router.get('/of/goods', (req, res) => {
 
 	// Available query params
-	// 
+	//
 	// goods_gid
-	// 
+	//
 
 	var _goods_gid = parseInt(req.query.goods_gid, 10);
 
-	users.hasMany(comments, {foreignKey: 'commenter_uid'});
-	comments.belongsTo(users, {foreignKey: 'commenter_uid'});
-
-	comments.findAll({
+	comments
+		.findAll({
 			where: {
 				goods_gid: _goods_gid
 			},
-			order: 'cid ASC',
+			order: 'cid DESC',
 			include: [{
 				model: users,
 				required: true
 			}]
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
-			res.send({
+		.catch(err => {
+			res.json({
 				error: err
 			});
 		});
 });
 
 // Get Comments of a user
-router.get('/of/user', function(req, res, next) {
-	
+router.get('/of/user', (req, res) => {
+
 	// Available query params
-	// 
+	//
 	// commenter_uid
-	// 
+	//
 
 	var _commenter_uid = parseInt(req.query.commenter_uid, 10);
 
-	comments.findAll({
+	comments
+		.findAll({
 			where: {
 				commenter_uid: _commenter_uid
-			}
+			},
+			order: 'cid DESC',
+			include: [{
+				model: goods,
+				required: true
+			}]
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
-			res.send({
+		.catch(err => {
+			res.json({
 				error: err
 			});
 		});
 });
 
 // Post a comment to a goods
-router.post('/post', function(req, res, next) {
+router.post('/post', (req, res) => {
 
 	// Necessary POST body params
-	// 
+	//
 	// goods_gid
 	// commenter_uid
-	// 
+	//
 
-	var _goods_gid     = parseInt(req.body.goods_gid, 10);
+	var _goods_gid = parseInt(req.body.goods_gid, 10);
 	var _commenter_uid = parseInt(req.body.commenter_uid, 10);
-	var _content       = req.body.content;
+	var _content = req.body.content;
 
-	comments.create({
+	comments
+		.create({
 			goods_gid: _goods_gid,
 			commenter_uid: _commenter_uid,
 			content: _content
 		})
-		.then(function(result) {
+		.then(result => {
 			if (result == null) {
 				res.json({});
 			} else {
 				res.json(result);
 			}
 		})
-		.catch(function(err) {
-			res.send({
+		.catch(err => {
+			res.json({
 				error: err
 			});
 		});
 });
 
 // Edit a comment
-router.put('/edit', function(req, res, next) {
+router.put('/edit', (req, res) => {
 
 	// Necessary PUT body params
-	// 
+	//
 	// cid
 	// content
-	// 
+	//
 
-	var _cid     = parseInt(req.body.cid, 10);
+	var _cid = parseInt(req.body.cid, 10);
 	var _content = req.body.content;
 
-	comments.findOne({
+	comments
+		.findOne({
 			where: {
 				cid: _cid
 			}
 		})
-		.then(function(result) {
+		.then(result => {
 			result.content = _content;
-			result.save().then(function() {});
+			result.save().then(() => {});
 			return result;
 		})
-		.then(function(result) {
+		.then(result => {
 			if (result == null) {
 				res.json(null);
 			} else {
 				res.json(result);
 			}
 		})
-		.catch(function(err) {
-			res.send({
+		.catch(err => {
+			res.json({
 				error: err
 			});
-			//res.send({error:err});
 		});
 });
 
 // Delete a comment
-router.delete('/delete', function(req, res, next) {
+router.delete('/delete', (req, res) => {
 
 	// Necessary DELETE query params
-	// 
+	//
 	// cid
 	//
 
 	var _cid = parseInt(req.query.cid, 10);
 
-	comments.destroy({
+	comments
+		.destroy({
 			where: {
 				cid: _cid
 			}
 		})
-		.then(function(result) {
+		.then(result => {
 			res.json(result);
 		})
-		.catch(function(err) {
-			res.send({
+		.catch(err => {
+			res.json({
 				error: err
 			});
 		});
