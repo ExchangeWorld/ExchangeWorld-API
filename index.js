585d5491963477d065ab5b100358fbf49da95704
NoelBao <noeleon930@gmail.com>
2015-11-01 08:42:45 +0800

    let server initiation be in the after-sync

--- index.js
+++ index.js
@@ -8,96 +8,95 @@ var cookieParser = require('cookie-parser');
 var compression = require('compression');
 
 var sequelize_sync = require('./libs/sync');
-
-var server = express();
-
-// log all requests to the console
-if (process.env.NODE_ENV !== 'production') {
-	server.use(morgan('dev'));
-}
-
-// for parsing application/json
-server.use(bodyParser.json({
-	limit: '64mb'
-}));
-
-// for parsing application/x-www-form-urlencoded
-server.use(bodyParser.urlencoded({
-	limit: '64mb',
-	extended: true
-}));
-
-server.use(cookieParser());
-server.use(compression());
-
-server.disable('x-powered-by');
-
-server.all('*', (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
-
-/* Token authentications:
- * If the path is not /api/authenticate, then it needs authentication
- * If fail, return {"authentication": "fail"}
- * If success, and then go next()
- */
-var authenticate = require('./routers/authenticate');
-server.all(/\/api\/(?!authenticate).+/, authenticate.token, (req, res, next) => {
-	next();
-});
-
-server.use('/api/goods/search', require('./routers/goods.search'));
-server.use('/api/goods', require('./routers/goods'));
-
-server.use('/api/upload', require('./routers/upload'));
-
-server.use('/api/user/profile/follower', require('./routers/follower'));
-server.use('/api/user/profile/following', require('./routers/following'));
-server.use('/api/user/profile', require('./routers/user.profile'));
-server.use('/api/user', require('./routers/user'));
-
-server.use('/api/comment', require('./routers/comment'));
-server.use('/api/star', require('./routers/star'));
-server.use('/api/queue', require('./routers/queue'));
-
-server.use('/api/exchange', require('./routers/exchange'));
-server.use('/api/notification', require('./routers/notification'));
-server.use('/api/message', require('./routers/message'));
-server.use('/api/chatroom', require('./routers/chatroom'));
-
-server.use('/api/authenticate', authenticate.router);
-
-// catch 404 and forward to error handler
-server.use((req, res, next) => {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// 500 error handlers
-// Development error handler: print stacktrace
-server.use((err, req, res, next) => {
-	res.status(err.status || 500);
-	err.statusCode = 500;
-	if (process.env.NODE_ENV !== 'production' && req.xhr) {
-		res.send(err);
-	} else if (req.xhr) {
-		res.send(err.message);
-	}
-
-	next(err);
-});
-
-// Sync DB first!
-// Then start webserver if not already running
 sequelize_sync
 	.then(() => {
 		console.log('DB all synced ...');
+		console.log('Igniting API server ...');
 	})
 	.then(() => {
+		var server = express();
+
+		// log all requests to the console
+		if (process.env.NODE_ENV !== 'production') {
+			server.use(morgan('dev'));
+		}
+
+		// for parsing application/json
+		server.use(bodyParser.json({
+			limit: '64mb'
+		}));
+
+		// for parsing application/x-www-form-urlencoded
+		server.use(bodyParser.urlencoded({
+			limit: '64mb',
+			extended: true
+		}));
+
+		server.use(cookieParser());
+		server.use(compression());
+
+		server.disable('x-powered-by');
+
+		server.all('*', (req, res, next) => {
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+			res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+			next();
+		});
+
+		/* Token authentications:
+		 * If the path is not /api/authenticate, then it needs authentication
+		 * If fail, return {"authentication": "fail"}
+		 * If success, and then go next()
+		 */
+		var authenticate = require('./routers/authenticate');
+		server.all(/\/api\/(?!authenticate).+/, authenticate.token, (req, res, next) => {
+			next();
+		});
+
+		server.use('/api/goods/search', require('./routers/goods.search'));
+		server.use('/api/goods', require('./routers/goods'));
+
+		server.use('/api/upload', require('./routers/upload'));
+
+		server.use('/api/user/profile/follower', require('./routers/follower'));
+		server.use('/api/user/profile/following', require('./routers/following'));
+		server.use('/api/user/profile', require('./routers/user.profile'));
+		server.use('/api/user', require('./routers/user'));
+
+		server.use('/api/comment', require('./routers/comment'));
+		server.use('/api/star', require('./routers/star'));
+		server.use('/api/queue', require('./routers/queue'));
+
+		server.use('/api/exchange', require('./routers/exchange'));
+		server.use('/api/notification', require('./routers/notification'));
+		server.use('/api/message', require('./routers/message'));
+		server.use('/api/chatroom', require('./routers/chatroom'));
+
+		server.use('/api/authenticate', authenticate.router);
+
+		// catch 404 and forward to error handler
+		server.use((req, res, next) => {
+			var err = new Error('Not Found');
+			err.status = 404;
+			next(err);
+		});
+
+		// 500 error handlers
+		// Development error handler: print stacktrace
+		server.use((err, req, res, next) => {
+			res.status(err.status || 500);
+			err.statusCode = 500;
+			if (process.env.NODE_ENV !== 'production' && req.xhr) {
+				res.send(err);
+			} else if (req.xhr) {
+				res.send(err.message);
+			}
+
+			next(err);
+		});
+
+		// Then start webserver if not already running
 		var s = http.createServer(server);
 		s.on('error', err => {
 			if (err.code === 'EADDRINUSE') {
