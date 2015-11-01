2bd05a628ec48c40a72458ce69f7259afb160eac
NoelBao <noeleon930@gmail.com>
2015-11-01 09:22:07 +0800

    fix comma bug

--- routers/message.js
+++ routers/message.js
@@ -38,11 +38,11 @@ router.get('/', (req, res) => {
 				],
 				include: [{
 					model: users,
-					as: 'sender'
+					as: 'sender',
 					required: true
 				}, {
 					model: users,
-					as: 'receiver'
+					as: 'receiver',
 					required: true
 				}],
 				// logging: true,
@@ -70,11 +70,11 @@ router.get('/', (req, res) => {
 				],
 				include: [{
 					model: users,
-					as: 'sender'
+					as: 'sender',
 					required: true
 				}, {
 					model: users,
-					as: 'receiver'
+					as: 'receiver',
 					required: true
 				}],
 				// logging: true,
