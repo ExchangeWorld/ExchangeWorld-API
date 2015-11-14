e5aa500589c6bd445137b02f385c44d755fad22e
noelBao <noeleon930@gmail.com>
2015-11-15 07:21:11 +0800

    change users' name to user_n

--- test/data/users.js
+++ test/data/users.js
@@ -1,20 +1,20 @@
 module.exports = [{
 	fb: false,
-	identity: 'fb_false',
+	identity: 'user_1',
 	password: '123',
 	name: 'User_1',
 	email: 'a@b.c',
 	photo_path: null
 }, {
 	fb: true,
-	identity: 'fb_true',
+	identity: 'user_2',
 	password: '123',
 	name: 'User_2',
 	email: 'a@b.c',
 	photo_path: null
 }, {
 	fb: false,
-	identity: 'fb_false2',
+	identity: 'user_3',
 	password: '123',
 	name: 'User_3',
 	email: 'a@b.c',
