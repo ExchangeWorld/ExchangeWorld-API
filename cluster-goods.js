4ee9f4fe33ed14ea87fcc155e51835d0ef06931a
NoelBao <noeleon930@gmail.com>
2016-01-31 19:45:15 +0800

    cluser level

--- cluster-goods.js
+++ cluster-goods.js
@@ -2,7 +2,7 @@
 const cluster_port = 3004;
 
 var cluster = require('cluster');
-var cpus = require('os').cpus().length;
+var cpus = require('os').cpus().length / 2;
 
 cluster.setMaxListeners(0);
 
