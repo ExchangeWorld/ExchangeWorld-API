e10122b58f2d6fcb244aeb2376471c7f69999698
NoelBao <noeleon930@gmail.com>
2016-02-06 11:54:27 +0800

    chatroom new schema

--- ORM/Chatrooms.js
+++ ORM/Chatrooms.js
@@ -7,7 +7,8 @@ var sequelize = require(path.resolve(__dirname, '../libs/sequelize'));
 /**
  * Define Chatrooms schema
  * @param  {Sequelize.BIGINT} cid Chatroom's ID
- * @param  {Sequelize.TEXT} members The members chatroom contains (in json)
+ * @param  {Sequelize.ARRAY(Sequelize.BIGINT)} members The members (user_uid) in this chatroom
+ * @param  {Sequelize.TEXT} last_message The last_message of this chatroom
  */
 var Chatrooms = sequelize.define('chatrooms', {
 	cid: {
@@ -18,10 +19,25 @@ var Chatrooms = sequelize.define('chatrooms', {
 		primaryKey: true
 	},
 	members: {
+		type: Sequelize.ARRAY(Sequelize.BIGINT),
+		defaultValue: [],
+		allowNull: false
+	},
+	last_message: {
 		type: Sequelize.TEXT,
 		defaultValue: '',
 		allowNull: false
 	}
+}, {
+	indexes: [{
+		unique: true,
+		fields: ['cid'],
+		method: 'BTREE'
+	}, {
+		fields: ['members'],
+		using: 'gin',
+		operator: '_int8_ops'
+	}]
 });
 
 module.exports = Chatrooms;
