6fb947069c7b1851c9a54d86dcbb22860f3588f5
NoelBao <noeleon930@gmail.com>
2016-02-19 00:20:58 +0800

    ttmp

--- cluster-websocket.js
+++ cluster-websocket.js
@@ -3,22 +3,7 @@
 const websocket_port = 3080;
 
 var path = require('path');
-// var cluster = require('cluster');
-// var cpus = require('os').cpus().length;
 
-// cluster.setMaxListeners(0);
-// process.setMaxListeners(0);
-
-// if (cluster.isMaster) {
-// 	for (var i = 0; i < cpus; i++) {
-// 		cluster.fork();
-// 	}
-//
-// 	cluster.on('exit', (worker, code, signal) => {
-// 		console.log('websocket-container worker %d died (%s). restarting...', worker.process.pid, signal || code);
-// 		cluster.fork();
-// 	});
-// } else {
 var sequelize_sync = require(path.resolve(__dirname, './libs/sync'));
 var websocketHandlers = require(path.resolve(__dirname, './websocket_handlers/main'));
 
@@ -31,4 +16,3 @@ sequelize_sync
 		var webSocketServerContainer = websocketHandlers.webSocketServerContainer;
 		webSocketServerContainer.listen(websocket_port);
 	});
-// }
