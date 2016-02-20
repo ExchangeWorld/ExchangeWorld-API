4b61c3c1fdfbd829ab7e601d51d67742be43b62c
NoelBao <noeleon930@gmail.com>
2016-02-21 01:52:20 +0800

    chatroom joining and leaving!

--- routers/chatroom.js
+++ routers/chatroom.js
@@ -16,31 +16,42 @@ var chatrooms = require(path.resolve(__dirname, '../ORM/Chatrooms'));
 // var messages = require(path.resolve(__dirname, '../ORM/Messages'));
 // var users = require(path.resolve(__dirname, '../ORM/Users'));
 
-// [{
-// 	cid: 1,
-// 	lastmessage: 'Hello?',
-// 	members: ['', '', '']
-// }, {
-// 	cid: 2,
-// 	lastmessage: 'Bye!',
-// 	members: ['', '', '']
-// }];
-
+/**
+ * Get a chatrooms meta of a user
+ *
+ * @method GET api/chatroom/of/user
+ * @param  {Integer} user_uid The uid who wants the meta (But session-based, so not required in normal mode)
+ * @param  {Integer=0} offset Retrive from which number of chatrooms (Like database offset)
+ * @param  {Integer=20} limit How many chatrooms to retrive (Like database limit)
+ * @return {JSON} Chatrooms meta
+ * @example
+<pre>
+[{
+	cid: 1,
+	lastmessage: 'Hello?',
+	members: [1, 3]
+}, {
+	cid: 2,
+	lastmessage: 'Bye!',
+	members: [2, 4, 6, 8]
+}];
+</pre>
+ */
 router.get('/of/user', (req, res) => {
-	var _chatter_uid;
+	var _user_uid;
 	var _offset = parseInt((req.query.offset || 0), 10);
 	var _limit = parseInt((req.query.limit || 20), 10);
 
 	// REQ EXWD CHECK
 	if (req.exwd.admin) {
-		_chatter_uid = parseInt(req.query.chatter_uid, 10);
+		_user_uid = parseInt(req.query.user_uid, 10);
 	} else if (req.exwd.anonymous) {
 		res.status(403).json({
 			error: 'Permission denied'
 		});
 		return;
 	} else if (req.exwd.registered) {
-		_chatter_uid = req.exwd.uid;
+		_user_uid = req.exwd.uid;
 	} else {
 		res.status(403).json({
 			error: 'Permission denied'
@@ -48,9 +59,9 @@ router.get('/of/user', (req, res) => {
 		return;
 	}
 
-	if (!_chatter_uid) {
+	if (!_user_uid) {
 		res.status(400).json({
-			error: 'chatter uid is not given'
+			error: 'user_uid is not given'
 		});
 		return;
 	}
@@ -59,7 +70,7 @@ router.get('/of/user', (req, res) => {
 		.findAll({
 			where: {
 				members: {
-					$contains: [_chatter_uid]
+					$contains: [_user_uid]
 				}
 			},
 			order: [
@@ -78,12 +89,175 @@ router.get('/of/user', (req, res) => {
 		});
 });
 
+/**
+ * Join a chatroom
+ *
+ * @method PUT api/chatroom/join
+ * @param  {Integer} cid The id of the room wanna join
+ * @param  {Integer} user_uid The id of the user who will join the chatroom (Can be others! Like invitation)
+ * @return {JSON} Modified chatroom object
+ */
+router.put('/join', (req, res) => {
+	var _cid = parseInt(req.body.cid, 10);
+	var _user_uid = parseInt(req.body.user_uid, 10);
+	var _requester_uid;
+
+	if (!_cid) {
+		res.status(400).json({
+			error: 'cid is not given'
+		});
+		return;
+	}
+
+	if (!_user_uid) {
+		res.status(400).json({
+			error: 'user_uid is not given'
+		});
+		return;
+	}
+
+	// REQ EXWD CHECK
+	if (req.exwd.admin) {
+		console.log('Nothing');
+	} else if (req.exwd.anonymous) {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	} else if (req.exwd.registered) {
+		_requester_uid = req.exwd.uid;
+	} else {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	}
+
+	var queryTmp = (req.exwd.admin ? {
+		where: {
+			cid: _cid
+		}
+	} : {
+		where: {
+			cid: _cid,
+			members: {
+				$contains: [_requester_uid]
+			}
+		}
+	});
+
+	chatrooms
+		.findOne(queryTmp)
+		.then(result => {
+			if (result) {
+				let tmpSet = new Set(result.members);
+				tmpSet.add(_user_uid);
+				result.members = Array.from(tmpSet);
+				return result.save();
+			}
+			return result;
+		})
+		.then(result => {
+			res.status(200).json(result);
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			});
+		});
+});
+
+/**
+ * Leave from a chatroom
+ *
+ * @method PUT api/chatroom/leave
+ * @param  {Integer} cid The id of the room wanna leave from
+ * @param  {Integer} user_uid The id of the user who will leave the chatroom
+ * @return {Integer} The number of handled chatroom(s) (Should be 1)
+ */
+router.put('/leave', (req, res) => {
+	var _cid = parseInt(req.body.cid, 10);
+	var _user_uid = parseInt(req.body.user_uid, 10);
+	var _requester_uid;
+
+	if (!_cid) {
+		res.status(400).json({
+			error: 'cid is not given'
+		});
+		return;
+	}
+
+	if (!_user_uid) {
+		res.status(400).json({
+			error: 'user_uid is not given'
+		});
+		return;
+	}
+
+	// REQ EXWD CHECK
+	if (req.exwd.admin) {
+		console.log('Nothing');
+	} else if (req.exwd.anonymous) {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	} else if (req.exwd.registered) {
+		_requester_uid = req.exwd.uid;
+	} else {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	}
+
+	var queryTmp = (req.exwd.admin ? {
+		where: {
+			cid: _cid
+		}
+	} : {
+		where: {
+			cid: _cid,
+			members: {
+				$contains: [_requester_uid]
+			}
+		}
+	});
+
+	chatrooms
+		.findOne(queryTmp)
+		.then(result => {
+			if (result) {
+				let tmpSet = new Set(result.members);
+				tmpSet.delete(_user_uid);
+				result.members = Array.from(tmpSet);
+				return result.save();
+			}
+			return result;
+		})
+		.then(result => {
+			res.status(200).json(result);
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			});
+		});
+});
+
+/**
+ * Create a chatroom
+ *
+ * @method POST api/chatroom/create
+ * @param  {String} members_json A JSON-Stringified members array like "[1,2,3,4]", at least two people
+ * @return {JSON} New created chatroom object
+ */
 router.post('/create', (req, res) => {
 	var _members_json = JSON.parse(req.body.members_json);
 
 	// REQ EXWD CHECK
 	if (req.exwd.admin) {
-
+		console.log('Nothing');
 	} else if (req.exwd.anonymous) {
 		res.status(403).json({
 			error: 'Permission denied'
@@ -102,10 +276,14 @@ router.post('/create', (req, res) => {
 
 	if (_members_json.length < 2) {
 		res.status(400).json({
-			error: 'members_json is too short'
+			error: 'members_json is too short, at least two'
 		});
 		return;
 	}
+
+	// Make it a Set !
+	var tmpSet = new Set(_members_json);
+	_members_json = Array.from(tmpSet);
 
 	chatrooms
 		.create({
@@ -121,28 +299,27 @@ router.post('/create', (req, res) => {
 		});
 });
 
-router.put('/join', (req, res) => {
-
-});
-
-router.put('/leave', (req, res) => {
-
-});
-
+/**
+ * Delete a chatroom
+ *
+ * @method DELETE api/chatroom/delete
+ * @param  {Integer} cid The id of the room going to be deleted
+ * @return {Integer} The number of processed chatroom(s) (Should be 1)
+ */
 router.delete('/delete', (req, res) => {
-	var _chatter_uid;
+	var _user_uid;
 	var _cid = parseInt(req.query.cid, 10);
 
 	// REQ EXWD CHECK
 	if (req.exwd.admin) {
-
+		console.log('Nothing');
 	} else if (req.exwd.anonymous) {
 		res.status(403).json({
 			error: 'Permission denied'
 		});
 		return;
 	} else if (req.exwd.registered) {
-		_chatter_uid = req.exwd.uid;
+		_user_uid = req.exwd.uid;
 	} else {
 		res.status(403).json({
 			error: 'Permission denied'
@@ -150,9 +327,9 @@ router.delete('/delete', (req, res) => {
 		return;
 	}
 
-	if ((!_cid) || (!_chatter_uid)) {
+	if ((!_cid) || (!_user_uid)) {
 		res.status(400).json({
-			error: 'cid or chatter_uid is wrong'
+			error: 'cid or user_uid is wrong'
 		});
 		return;
 	}
@@ -165,7 +342,7 @@ router.delete('/delete', (req, res) => {
 		where: {
 			cid: _cid,
 			members: {
-				$contains: [_chatter_uid]
+				$contains: [_user_uid]
 			}
 		}
 	});
