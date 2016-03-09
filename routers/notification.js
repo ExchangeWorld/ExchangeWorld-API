caca4d3e4ed1f137751a1d75ee8ffbe711a56b6c
NoelBao <noeleon930@gmail.com>
2016-03-09 17:30:00 +0800

    notification new router

--- routers/notification.js
+++ routers/notification.js
@@ -6,30 +6,45 @@
 
 'use strict';
 
+var path = require('path');
+
 var express = require('express');
 var router = express.Router();
 
 // Including tables
-var notifications = require('../ORM/Notifications');
-var users = require('../ORM/Users');
+var notifications = require(path(__dirname, '../ORM/Notifications'));
+var users = require(path(__dirname, '../ORM/Users'));
 
 /**
  * Get notifications belong to a user
  *
  * @method GET api/notification/belongsTo
  * @param  {Integer} receiver_uid
- * @param  {Integer=0} from
- * @param  {Integer=10} number
- * @return {JSON} The notifications including receiver and sender
+ * @param  {Integer=0} offset
+ * @param  {Integer=20} limit
+ * @return {Array} The notifications including receiver
  */
 router.get('/belongsTo', (req, res) => {
+	var _receiver_uid;
+	var _offset = parseInt(req.query.offset || 0, 10);
+	var _limit = parseInt(req.query.limit || 20, 10);
 
-	var _receiver_uid = parseInt(req.query.receiver_uid, 10);
-	var _from = parseInt(req.query.from, 10);
-	var _number = parseInt(req.query.number, 10);
-
-	_from = (_from == _from ? _from : 0);
-	_number = (_number == _number ? _number : 10);
+	// REQ EXWD CHECK
+	if (req.exwd.admin) {
+		_receiver_uid = parseInt(req.query.receiver_uid, 10);
+	} else if (req.exwd.anonymous) {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	} else if (req.exwd.registered) {
+		_receiver_uid = req.exwd.uid;
+	} else {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	}
 
 	notifications
 		.findAll({
@@ -39,93 +54,76 @@ router.get('/belongsTo', (req, res) => {
 			order: [
 				['nid', 'DESC']
 			],
-			include: [{
-				model: users,
-				as: 'sender',
-				required: true
-			}, {
-				model: users,
-				as: 'receiver',
-				required: true
-			}],
-			offset: _from,
-			limit: _number
+			offset: _offset,
+			limit: _limit
 		})
 		.then(result => {
-			res.json(result);
+			res.status(200).json(result);
 		})
 		.catch(err => {
-			res.send({
+			res.status(500).json({
 				error: err
 			});
 		});
 });
 
 /**
- * Get notification by given nid
+ * Get a notification by given nid
  *
  * @method GET api/notification
  * @param  {Integer} nid The ID of notification
- * @return {JSON} A notification including receiver and sender
+ * @return {JSON} A notification including receiver
  */
 router.get('/', (req, res) => {
-
 	var _nid = parseInt(req.query.nid, 10);
+
+	var _requester_uid;
+
+	if (!_nid) {
+		res.status(400).json({
+			error: 'nid is not given'
+		});
+		return;
+	}
+
+	// REQ EXWD CHECK
+	if (req.exwd.admin) {
+		console.log('Nothing');
+	} else if (req.exwd.anonymous) {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	} else if (req.exwd.registered) {
+		_requester_uid = req.exwd.uid;
+	} else {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	}
+
+	var queryTmp = (req.exwd.admin ? {
+		nid: _nid
+	} : {
+		nid: _nid,
+		receiver_uid: _requester_uid
+	});
 
 	notifications
 		.findOne({
-			where: {
-				nid: _nid
-			},
+			where: queryTmp,
 			include: [{
-				model: users,
-				as: 'sender',
-				required: true
-			}, {
 				model: users,
 				as: 'receiver',
 				required: true
 			}]
 		})
 		.then(result => {
-			res.json(result);
+			res.status(200).json(result);
 		})
 		.catch(err => {
-			res.send({
-				error: err
-			});
-		});
-});
-
-/**
- * Emit notification
- *
- * @method POST api/notification/post
- * @param  {Integer} sender_uid The user trigger the notification
- * @param  {Integer} receiver_uid Target user
- * @param  {String} trigger_url The trigger object's URL
- * @param  {String} content The content of the notification
- * @return {JSON} A notification including receiver and sender
- */
-router.post('/post', (req, res) => {
-
-	var _sender_uid = parseInt(req.body.sender_uid);
-	var _receiver_uid = parseInt(req.body.receiver_uid);
-	var _trigger_url = req.body.trigger_url;
-	var _content = req.body.content;
-
-	notifications
-		.create({
-			sender_uid: _sender_uid,
-			receiver_uid: _receiver_uid,
-			trigger_url: _trigger_url,
-			content: _content
-		})
-		.then(result => {
-			res.json(result);
-		})
-		.catch(err => {
-			res.send({
+			res.status(500).json({
 				error: err
 			});
 		});
@@ -139,22 +137,60 @@ router.post('/post', (req, res) => {
  * @return {JSON} Updated notification object
  */
 router.put('/read', (req, res) => {
+	var _nid = parseInt(req.query.nid, 10);
 
-	var _nid = parseInt(req.body.nid, 10);
+	var _requester_uid;
+
+	if (!_nid) {
+		res.status(400).json({
+			error: 'nid is not given'
+		});
+		return;
+	}
+
+	// REQ EXWD CHECK
+	if (req.exwd.admin) {
+		console.log('Nothing');
+	} else if (req.exwd.anonymous) {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	} else if (req.exwd.registered) {
+		_requester_uid = req.exwd.uid;
+	} else {
+		res.status(403).json({
+			error: 'Permission denied'
+		});
+		return;
+	}
+
+	var queryTmp = (req.exwd.admin ? {
+		nid: _nid,
+		read: false
+	} : {
+		nid: _nid,
+		read: false,
+		receiver_uid: _requester_uid
+	});
 
 	notifications
-		.update({
-			unread: false
-		}, {
-			where: {
-				nid: _nid
-			}
+		.findOne({
+			where: queryTmp
 		})
 		.then(result => {
-			res.json(result);
+			if (result) {
+				result.read = true;
+				return result.save();
+			}
+
+			return result;
+		})
+		.then(result => {
+			res.status(200).json(result);
 		})
 		.catch(err => {
-			res.send({
+			res.status(500).json({
 				error: err
 			});
 		});
