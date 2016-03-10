723936796b2404d5b15eebba3ad38aa1d282c95b
NoelBao <noeleon930@gmail.com>
2016-03-10 11:32:22 +0800

    complete vert first notification_dispatcher

--- websocket_handlers/notification_dispatcher.js
+++ websocket_handlers/notification_dispatcher.js
@@ -6,11 +6,12 @@ var comments = require(path.resolve(__dirname, '../ORM/Comments'));
 var exchanges = require(path.resolve(__dirname, '../ORM/Exchanges'));
 var follows = require(path.resolve(__dirname, '../ORM/Follows'));
 var goods = require(path.resolve(__dirname, '../ORM/Goods'));
+var notifications = require(path.resolve(__dirname, '../ORM/Notifications'));
 var queues = require(path.resolve(__dirname, '../ORM/Queues'));
 var stars = require(path.resolve(__dirname, '../ORM/Stars'));
 var users = require(path.resolve(__dirname, '../ORM/Users'));
 
-var redis_sub = require(path.resolve(__dirname, '../libs/redis_sub'));
+// var redis_sub = require(path.resolve(__dirname, '../libs/redis_sub'));
 
 // There can be new comments, new exchanges, new follows, new goods, new queues, new stars.
 
@@ -23,30 +24,27 @@ var handleComments = msgObj => {
 			include: [{
 				model: users,
 				as: 'commenter',
-				required: true,
 				attributes: ['uid', 'name', 'photo_path']
 			}, {
 				model: goods,
 				as: 'goods',
-				where: {
-					deleted: 0
-				},
-				required: true,
 				attributes: ['gid', 'owner_uid', 'name', 'photo_path'],
 				include: [{
 					model: stars,
-					as: 'star_goods',
-					required: true
+					as: 'star_goods'
 				}]
 			}]
 		})
 		.then(result => {
 			if (result) {
+				var _result_json = result.toJSON();
+
 				var _tmp = [
 					[result.goods.owner_uid, {
 						codeType: 20001,
 						payload: {
-							comment: result.toJSON()
+							goods: _result_json.goods,
+							comment: _result_json
 						}
 					}]
 				];
@@ -54,7 +52,8 @@ var handleComments = msgObj => {
 				_tmp = _tmp.concat(result.goods.star_goods.map(s => [s.starring_user_uid, {
 					codeType: 20004,
 					payload: {
-						comment: result.toJSON()
+						goods: _result_json.goods,
+						comment: _result_json
 					}
 				}]));
 
@@ -66,7 +65,40 @@ var handleComments = msgObj => {
 };
 
 var handleExchanges = msgObj => {
+	// msgObj.whoNeedsNotification
 
+	return exchanges
+		.findOne({
+			where: {
+				eid: msgObj.id
+			},
+			include: [{
+				model: goods,
+				as: 'goods_one'
+			}, {
+				model: goods,
+				as: 'goods_two'
+			}]
+		})
+		.then(result => {
+			if (result) {
+				var _tmp = [
+					[msgObj.whoNeedsNotification, {
+						codeType: 30001,
+						payload: {
+							exchange: result.toJSON()
+						}
+					}]
+				];
+
+				// codeType: 30002 PENDING!
+				// codeType: 30003 PENDING!
+
+				return _tmp;
+			}
+
+			return [];
+		});
 };
 
 var handleFollows = msgObj => {
@@ -77,10 +109,10 @@ var handleFollows = msgObj => {
 			},
 			include: [{
 				model: users,
-				as: 'follows_follower'
+				as: 'follower'
 			}, {
 				model: users,
-				as: 'follows_followed'
+				as: 'followed'
 			}]
 		})
 		.then(result => {
@@ -89,7 +121,7 @@ var handleFollows = msgObj => {
 					[result.followed_uid, {
 						codeType: 10004,
 						payload: {
-							person: (result.toJSON()).follows_follower
+							person: (result.toJSON()).follower
 						}
 					}]
 				];
@@ -103,15 +135,92 @@ var handleFollows = msgObj => {
 };
 
 var handleGoods = msgObj => {
+	return goods
+		.findOne({
+			where: {
+				gid: msgObj.id
+			},
+			include: [{
+				model: users,
+				as: 'owner',
+				include: [{
+					model: follows,
+					as: 'follows_follower'
+				}, {
+					model: follows,
+					as: 'follows_followed'
+				}]
+			}]
+		})
+		.then(result => {
+			var _tmp = [];
 
+			_tmp = _tmp.concat(result.owner.follows_follower.map(f => [f.follower_uid, {
+				codeType: 10001,
+				payload: {
+					goods: result.toJSON()
+				}
+			}]));
+
+			// codeType: 10002 PENDING!
+			return _tmp;
+		});
 };
 
 var handleQueues = msgObj => {
+	return queues
+		.findOne({
+			where: {
+				qid: msgObj.id
+			},
+			include: [{
+				model: goods,
+				as: 'host_goods'
+			}, {
+				model: goods,
+				as: 'queuer_goods'
+			}]
+		})
+		.then(result => {
+			var _tmp = [
+				[result.host_goods.owner_uid, {
+					codeType: 20002,
+					payload: {
+						queue: result.toJSON()
+					}
+				}]
+			];
 
+			return _tmp;
+		});
 };
 
 var handleStars = msgObj => {
+	return stars
+		.findOne({
+			where: {
+				sid: msgObj.id
+			},
+			include: [{
+				model: goods,
+				as: 'goods'
+			}, {
+				model: users,
+				as: 'starring_user'
+			}]
+		})
+		.then(result => {
+			var _tmp = [
+				[result.goods.owner_uid, {
+					codeType: 20003,
+					payload: {
+						person: (result.toJSON()).starring_user
+					}
+				}]
+			];
 
+			return _tmp;
+		});
 };
 
 var modelHandlers = {
@@ -126,14 +235,35 @@ var modelHandlers = {
 var subscribedMessageHandler = (channel, msg) => {
 	if (channel === 'notifications') {
 		var msgObj = JSON.parse(msg);
+
+		return modelHandlers[msgObj.model]
+			.then(pushingTargetsAndPayloads => {
+				if (pushingTargetsAndPayloads.length > 0) {
+					var _tmp = pushingTargetsAndPayloads
+						.map(_arr => ({
+							receiver_uid: _arr[0],
+							body: _arr[1]
+						}));
+
+					return notifications
+						.bulkCreate(_tmp)
+						.then(() => {
+							return pushingTargetsAndPayloads;
+						});
+				}
+
+				return pushingTargetsAndPayloads;
+			});
 	} else {
 		console.log(channel, 'is not subscribed ...');
 	}
 };
 
-redis_sub.subscribe('notifications');
-redis_sub.on('message', (channel, msg) => {
-	if (channel === 'notifications') {
-		return;
-	}
-});
+// redis_sub.subscribe('notifications');
+// redis_sub.on('message', (channel, msg) => {
+// 	if (channel === 'notifications') {
+// 		return;
+// 	}
+// });
+
+module.exports = subscribedMessageHandler;
